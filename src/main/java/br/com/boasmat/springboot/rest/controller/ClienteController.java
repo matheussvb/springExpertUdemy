e2f7566392d60127ba34f433119af1cda25dd257
Matheus <matheus.svb@hotmail.com>
2020-04-21 14:32:11 -0300

    get iniciado

--- src/main/java/br/com/boasmat/springboot/rest/controller/ClienteController.java
+++ src/main/java/br/com/boasmat/springboot/rest/controller/ClienteController.java
@@ -1,16 +1,38 @@
 package br.com.boasmat.springboot.rest.controller;
 
-import org.springframework.web.bind.annotation.*;
+import br.com.boasmat.springboot.domain.entity.Cliente;
+import br.com.boasmat.springboot.domain.repository.ClientesRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/clientes")
+//@RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nome}",
-                    method = RequestMethod.GET,
-                    consumes = )
+    @Autowired
+    private ClientesRespository clientesRespository;
+
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente) {
-        return String.format("Hello %s ", nomeCliente);
+    @GetMapping("/api/clientes/{id}")
+    public ResponseEntity getClienteById(@PathVariable("id") Integer id) {
+        Optional<Cliente> cliente = clientesRespository.findById(id);
+        if (cliente.isPresent()) {
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.set("Authorization", "token");
+            ResponseEntity<Cliente> responseEntity =
+                    new ResponseEntity<Cliente>(cliente.get(), httpHeaders, HttpStatus.OK);
+
+            return ResponseEntity.ok(cliente.get());
+        }
+        return ResponseEntity.notFound().build();
     }
+
 }
